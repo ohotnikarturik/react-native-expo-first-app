5505e8d848badbb1cc28417165c70e97aa7aa87c
Okhotnichenko Artur <ohotnikarturik@gmail.com>
2020-08-29 19:47:51 +0300

    buttons input reset, confirm

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,23 +1,87 @@
-import React from "react";
-import { View, Text, StyleSheet, TextInput, Button } from "react-native";
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from "react-native";
 
-import Card from '../components/Card'
-import Colors from '../constant/colors'
-
+import Card from "../components/Card";
+import Colors from "../constant/colors";
+import Input from "../components/Input";
 
 export const StartGameScreen = ({ title }) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [confirmed, setConfirmed] = useState(false);
+  const [selectedNumber, setSelectedNumber] = useState();
+
+  const numerInputHandler = (inputText) => {
+    setEnteredValue(inputText.replace(/[^0-9]/g, ""));
+  };
+
+  const resetInputHandler = () => {
+    setEnteredValue("");
+    setConfirmed(false);
+  };
+
+  const confirmInputHandler = () => {
+    const chooseNumber = parseInt(enteredValue);
+    if (chooseNumber === NaN || chooseNumber <= 0 || chooseNumber > 99) {
+      return;
+    }
+    setConfirmed(true);
+    setSelectedNumber(chooseNumber);
+    setEnteredValue("");
+  };
+
+  let confirmedInputOutput;
+
+  if (confirmed)
+    confirmedInputOutput = <Text>Chosen nunber: {selectedNumber}</Text>;
+
   return (
-    <View style={styles.screen}>
-      <Text style={styles.title}>The Game Screen</Text>
-      <Card style={styles.inputContainer}>
-        <Text style={styles.inputTitle}>Select a number</Text>
-        <TextInput style={styles.input} />
-        <View style={styles.buttonContainer}>
-          <View style={styles.button}><Button title={'Reset'} onPress={() => {}} color={Colors.red} /></View>
-          <View style={styles.button}><Button title={'Confirm'} onPress={() => {}} color={Colors.primary} /></View>
-        </View>
-      </Card>
-    </View>
+    <TouchableWithoutFeedback
+      onPress={() => {
+        Keyboard.dismiss();
+      }}
+    >
+      <View style={styles.screen}>
+        <Text style={styles.title}>The Game Screen</Text>
+        <Card style={styles.inputContainer}>
+          <Text style={styles.inputTitle}>Select a number</Text>
+          <Input
+            style={styles.input}
+            blurOnSubmit
+            placeholder="Enter..."
+            keyboardType="number-pad"
+            autoCorrect={false}
+            autoCapitalize="none"
+            maxLength={2}
+            onChangeText={numerInputHandler}
+            value={enteredValue}
+          />
+          <View style={styles.buttonContainer}>
+            <View style={styles.button}>
+              <Button
+                title={"Reset"}
+                onPress={resetInputHandler}
+                color={Colors.red}
+              />
+            </View>
+            <View style={styles.button}>
+              <Button
+                title={"Confirm"}
+                onPress={confirmInputHandler}
+                color={Colors.primary}
+              />
+            </View>
+          </View>
+        </Card>
+        {confirmedInputOutput}
+      </View>
+    </TouchableWithoutFeedback>
   );
 };
 
@@ -25,7 +89,7 @@ const styles = StyleSheet.create({
   screen: {
     flex: 1,
     padding: 10,
-    alignItems: 'center',
+    alignItems: "center",
   },
   title: {
     fontSize: 20,
@@ -33,24 +97,25 @@ const styles = StyleSheet.create({
   },
   inputContainer: {
     width: 300,
-    maxWidth: '80%',
+    maxWidth: "80%",
     alignItems: "center",
   },
   inputTitle: {
     fontSize: 18,
-    marginBottom: 10
-  },
-  input: {
-    padding: 10,
-    width: '100%'
+    marginBottom: 10,
   },
   buttonContainer: {
-    flexDirection: 'row',
-    width: '100%',
-    justifyContent: 'space-between',
-    paddingHorizontal: 15
+    flexDirection: "row",
+    width: "100%",
+    justifyContent: "space-between",
+    paddingHorizontal: 15,
   },
   button: {
-    width: 100
-  }
+    width: 100,
+  },
+  input: {
+    width: 100,
+    textAlign: "center",
+    fontSize: 20,
+  },
 });
